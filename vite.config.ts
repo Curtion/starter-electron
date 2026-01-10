1ec4d8c45604e41530ea0776adbdb52a6e13af2a
Curtion <curtion@126.com>
2026-01-10 23:36:04 +0800

    feat: 添加unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import { electronDevPlugin } from './vite-plugins/electron'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(), electronDevPlugin()],
+  plugins: [vue(), UnoCSS(), electronDevPlugin()],
   build: {
     outDir: 'dist-web',
   },
