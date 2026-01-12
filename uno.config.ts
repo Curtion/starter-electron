df6fa4d13381e37974e4d37baf9c31f81203cbba
Curtion <curtion@126.com>
2026-01-12 11:24:09 +0800

    feat: 新增图标

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig, presetAttributify, presetWind3 } from 'unocss'
+import { defineConfig, presetAttributify, presetIcons, presetWind3 } from 'unocss'
 
 export default defineConfig({
   presets: [
     presetAttributify({}),
     presetWind3(),
+    presetIcons({}),
   ],
 })
